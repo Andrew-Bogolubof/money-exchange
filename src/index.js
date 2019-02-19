d09fdaeb8d2acc4405f3aa3f18d66bf7ace3a92c
Andrew-Bogolubof <a.bogolubof@gmail.com>
2019-02-19 18:15:53 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var result = {};
+    if (currency >= 10000)
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    if (currency >= 50) {
+        result["H"] = Math.floor( currency / 50 );
+        currency %= 50;
+        // console.log(currency);
+    }
+    if (currency >= 25) {
+        result["Q"] = Math.floor( currency / 25 );
+        currency %= 25;
+        // console.log(currency);
+    }
+    if (currency >= 10) {
+        result["D"] = Math.floor( currency / 10 );
+        currency %= 10;
+        // console.log(currency);
+    }
+    if (currency >= 5) {
+        result["N"] = Math.floor( currency / 5 );
+        currency %= 5;
+        // console.log(currency);
+    }
+    if (currency >= 1) {
+        result["P"] = currency;
+        // console.log(currency);
+    }
+    if (currency < 1) {
+        return result;
+    }
+    return result;
 }
